a6d560fcb1514782703219ee64eaf476a9d1c5cf
Angela Purcell <kalmpurcell@gmail.com>
2021-05-20 23:51:46 -0700

    added exit function

--- quizgame.go
+++ quizgame.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	// "flag"
+	"fmt"
+	"os"
+	// "strings"
+)
 
-func main (){
-	fmt.Println("using this package")
+// func NewReader(r io.Reader) *Reader why is this different than the docs??
+func getProblemFile() {
+	// var file string 
+	// fmt.Scan(&file)
+	fmt.Println("what's broken")
+
+
+}
+
+func parseProblemFile(file string){
+
+	f, err := os.Open(file)
+	if err != nil {
+		//  the %s prints a plain string 
+		exit(fmt.Sprintf("Failed to open the CSV file: %s", file))
+	}
+	reader := csv.NewReader(f)
+	records, _ := reader.ReadAll()
+	if err != nil {
+		exit("Failed to parse the provided CSV file.")
+	}
+	fmt.Println(records)
+}
+
+func main () {
+	parseProblemFile("problems.csv")
+	getProblemFile()
+	// parseProblemFile()
+
+
+
+	// we need a filename variable so user can customize problems filename
+
+}
+//  https://stackoverflow.com/questions/12907653/what-is-the-difference-between-string-and-string-in-golang
+//  Link on why the brackets are there......could use more explanation here 
+func parseLines (lines [][]string) []problem{
+	
+}
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
 }
